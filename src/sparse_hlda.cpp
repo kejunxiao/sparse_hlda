5d368e01bdd4ad551e2fd69afd890377f2a32590
xiaokejun <xiaokejun@merger138.se.bjyt.qihoo.net>
2020-07-01 12:36:54 +0800

    fix load data error

--- src/sparse_hlda.cpp
+++ src/sparse_hlda.cpp
@@ -168,7 +168,7 @@ void learnVocabFromDocs() {
                 }
             }
         } else { // append ch to buf
-            if (len >= MAX_STRING) continue;
+            if (len == MAX_STRING - 1) continue;
             buf[len] = ch;
             len++;
         }
